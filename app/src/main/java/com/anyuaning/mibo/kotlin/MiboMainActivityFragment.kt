e23af57cf63d9155d87bf3959f34ca03cee0c339
Thomsen <changsen4584@163.com>
2016-03-17 19:11:44 +0800

    mibo kotlin add android extensions plugin

--- app/src/main/java/com/anyuaning/mibo/kotlin/MiboMainActivityFragment.kt
+++ app/src/main/java/com/anyuaning/mibo/kotlin/MiboMainActivityFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_mibo_main.*
 
 /**
  * A placeholder fragment containing a simple view.
@@ -14,5 +15,10 @@ class MiboMainActivityFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                      savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_mibo_main, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        activity.tv_hello.setText("Hello Kotlin extensions");
     }
 }
